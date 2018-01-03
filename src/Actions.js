4250168f9318ae8054c1ed8d55d73768d60652fc
Patrick Sollars <pjsollars@gmail.com>
2018-01-03 12:04:04 -0500

    adds map method to Board

--- src/Actions.js
+++ src/Actions.js
@@ -1,3 +1,18 @@
+
+export const createNote = () => {
+    alert("create note");
+};
+
+export const updateNote = () => {
+    alert("update note");
+};
+
+export const deleteNote = () => {
+    alert("delete note");
+};
+
 export const getNotes = () => {
     alert("getting notes");
 };
+
+export default getNotes;
--- src/Board.js
+++ src/Board.js
@@ -6,18 +6,29 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      notes: [
+        "go to grocery store",
+        "shovel driveway",
+        "play zelda",
+        "listen to radiohead"
+      ]
       
     };
   }
 
-
   render() {
+    const notes = this.state.notes.map((note, i) => {
+      return <Note key={i}>{note}</Note>;
+    });
+
     return (
       <div className="board">
-
+        {notes}
+        <button onClick={getNotes}>getNotes</button>
       </div>
     );
   }
+
 
 
 }
