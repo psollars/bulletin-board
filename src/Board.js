172e7286a3c21b81da7228046a011b584605b94a
Patrick Sollars <pjsollars@gmail.com>
2018-01-03 13:01:01 -0500

    refactors note map function

--- src/Board.js
+++ src/Board.js
@@ -7,27 +7,30 @@ class Board extends Component {
     super(props);
     this.state = {
       notes: [
-        "go to grocery store",
-        "shovel driveway",
-        "play zelda",
-        "listen to radiohead"
-      ]
-      
+        {id: 0, text: "go to grocery store"},
+        {id: 1, text: "shovel driveway"},
+        {id: 2, text: "play zelda"},
+        {id: 3, text: "listen to radiohead"}
+      ] 
     };
   }
 
   render() {
-    const notes = this.state.notes.map((note, i) => {
-      return <Note key={i}>{note}</Note>;
-    });
-
     return (
       <div className="board">
-        {notes}
+        {this.state.notes.map(this.eachNote)}
         <button onClick={getNotes}>getNotes</button>
       </div>
     );
   }
+
+  eachNote = (note) => {
+    return (<Note key={note.id}
+                  id={note.id}>
+              {note.text}
+            </Note>);
+  };
+
 
 
 
